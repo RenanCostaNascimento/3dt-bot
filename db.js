d0006db40ae7fae8de8ac510f7e076c1591147d5
Renan Nascimento <Renan@costanascimento.com.br>
2020-04-08 20:20:52 -0300

    added db helper

---
+++ db.js
@@ -0,0 +1,52 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const { MONGO_DB_DATABASE, MONGO_DB_URL } = process.env;
+
+// Connection URL
+const url = MONGO_DB_URL;
+
+// Database Name
+const dbName = MONGO_DB_DATABASE;
+
+// Create a new MongoClient
+const client = new MongoClient(url, { useUnifiedTopology: true });
+client.connect((err) => {
+  if (err) {
+    console.log('erro de conexão', err);
+  }
+});
+
+const inserirFicha = (ficha) => {
+  return new Promise((resolve, reject) => {
+    const db = client.db(dbName);
+    db.collection('fichas').replaceOne(
+      { jogador: ficha.jogador, canal: ficha.canal, nome: ficha.nome },
+      ficha,
+      { upsert: true },
+      (err, result) => {
+        if (err) { console.log('erro ao inserir', err); return reject(err); }
+        resolve(result);
+      });
+  });
+};
+
+const buscarFicha = (jogador, canal) => {
+  return new Promise((resolve, reject) => {
+    const db = client.db(dbName);
+    db.collection('fichas').findOne(
+      { jogador: jogador, canal: canal },
+      (err, result) => {
+        if (err) { console.log('erro ao buscar', err); return reject(err); }
+        resolve(result);
+      });
+  });
+};
+
+const mongoHelper = {
+  inserirFicha,
+  buscarFicha
+};
+
+Object.freeze(mongoHelper);
+
+module.exports = mongoHelper;
